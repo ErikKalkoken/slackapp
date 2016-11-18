6368200aa908ce83124818f3e4d1fabb4a026091
Erik Kalkoken <erik.kalkoken@gmail.com>
2016-11-18 16:17:22 +0100

    removed SSL and redirect

--- install.php
+++ install.php
@@ -5,31 +5,46 @@
  * Copyright (C) 2016 Erik Kalkoken
  *
  * Example script for installation a Slack app
- * This is a rudimentary example for a script that installs a Slack app with OAuth
  *
  * HISTORY
+ * 18-NOV-2016 v1.1 Additional simplifications
  * 17-NOV-2016 v1.0 Initial based on installer script for evetools
  *
 **/
 
-//////////// const
-const APP_NAME = "Example App";
-
 //////////// functions
 
-//////////// initialization
+// Show consistent user message when installation process failed for any reason
+// And allow restart of installation process
+function showInstallFailed ($msg)
+{
+	$myScriptName = pathinfo (__FILE__, PATHINFO_BASENAME);
+	
+	echo '<p>' . $msg. '</p>';
+	echo '<p>Installation failed - click <a href="' . $myScriptName. '">here</a> to start over</p>';	
+}
+
+//////////// Custom app parameters
+
+// app name
+const APP_NAME = "Example App";
 
 // Slack OAuth paramter
+// These are the basic parameters to identify your Slack app
+// They can be found in the Slack app adminstration settings under "Basic Information"
 $client_id = "xx";
 $client_secret = "xx";
 
 // Define scopes to be requested
-$scope = "commands";
+// Here we request "commands" to be able to add our own slash commands 
+// and "chat.write.user" which allows us to post messages on behalf of the installing user
+$scope = "commands,chat:write:user";
+
+
+//////////// main
 
 // get filename of this script
 $myScriptName = pathinfo (__FILE__, PATHINFO_BASENAME);
-
-//////////// main
 
 // get value of URL parameters from request
 // will be NULL if parameter is not set
@@ -38,8 +53,6 @@ $input['code'] = filter_input (INPUT_GET, "code", FILTER_SANITIZE_STRING);
 $input['state'] = filter_input (INPUT_GET, "state", FILTER_SANITIZE_STRING);
 $input['error'] = filter_input (INPUT_GET, "error", FILTER_SANITIZE_STRING);
 
-
-//////////// main
 
 // start output to browser
 
@@ -56,20 +69,15 @@ echo '
 
 //////////////////////////////////////////////////////
 // Page: Startpage
-//
 // This page is shown at the begining
 // It contains the "Add to Slack" button 
 //////////////////////////////////////////////////////
 
 if ( ($input['code'] === null) && ($input['error'] === null) )
 {	
-	// build re-direct URI
-	$redirect_uri = (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on')) ? 'https://' : 'http://';
-	$redirect_uri .= $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']);
-	$redirect_uri .= "/". $myScriptName;
-	
-	$slack_call = "https://slack.com/oauth/authorize?scope=$scope";
-	$slack_call .= "&client_id=$client_id&redirect_uri=$redirect_uri";
+	$slack_call = "https://slack.com/oauth/authorize";
+	$slack_call .= "?scope=$scope";
+	$slack_call .= "&client_id=$client_id";
 	
 	echo '
 		<h1>Installing ' . APP_NAME . ' for your Slack team</h1>
@@ -82,13 +90,11 @@ if ( ($input['code'] === null) && ($input['error'] === null) )
 		<p><a href="' . $slack_call . '">
 		<img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a></p>
 		<br>
-		
-		';
+	';
 }
 
 //////////////////////////////////////////////////////
 // Page: Processing of access code from Slack 
-//
 // Slack has called this script with the code parameter which contains the access code
 //////////////////////////////////////////////////////
 
@@ -119,26 +125,32 @@ elseif ( $input['code'] !== null )
 		// the curl request failed
 		$curlerror = curl_error($ch);
 		$msg = "Error while trying to access the slack API - Curl error: '$curlerror' - http error: '$http_code'";
-		echo '<p>' . $msg. '</p>';
-		echo '<p>Installation failed - click <a href="' . $myScriptName. '">here</a> to start over</p>';
+		showInstallFailed ($msg);
 	}
 	else
 	{
 		$response = json_decode($result, true);
 		
+		// Check if json_decode suceeded
+		if ($response === null) 
+		{
+			$msg = "Auth process could not be completed successfully. Response from Slack was: '$response'";
+			showInstallFailed ($msg);
+		}
 		// Check if Slack returned OK for the auth process
-		if ($response['ok'] === false)
+		elseif ($response['ok'] === false)
 		{
 			$errorMsg = isset ($response['error']) ? $response['error'] : "no error message";
 			$msg = "Auth process could not be completed successfully. Response from Slack was: '$errorMsg'";
-			echo '<p>' . $msg. '</p>';			
-			echo '<p>Installation failed - click <a href="' . $myScriptName. '">here</a> to start over</p>';
+			showInstallFailed ($msg);
 		}
 		else
 		{
+			// Installation process has suceeded
+			
 			// $response contains the access token, information about the Slack team
 			// additional information based on the requested scope
-			//
+			
 			// here is an example for how the response will look:
 			//
 			//	{
@@ -161,24 +173,23 @@ elseif ( $input['code'] !== null )
 			// Add some code here to further process and store the received tokens and information from Slack
 					
 			$teamName = $response['team_name'];
-			echo '<h2>Installation completed successfully.</h2>
-			<p>Thank you for installing ' . APP_NAME . ' to your Slack team ' . $teamName. '</p>
-			<p>You can now close this browser window and return to Slack to try out the new app.</p>';
+			echo '
+				<h2>Installation completed successfully.</h2>
+				<p>Thank you for installing ' . APP_NAME . ' to your Slack team ' . $teamName. '</p>
+				<p>You can now close this browser window and return to Slack to try out the new app.</p>
+			';
 		}
 	}
 }
 
 //////////////////////////////////////////////////////
 // Page: Processing of errors returned from Slack (Hidden)
-//
-// This part will be exuectued if Slack returns an error code
 //////////////////////////////////////////////////////
 
 elseif ( $input['error'] !== null )
 {
 	$msg = "Received an error from Slack: '" . $input['error'] . "'";
-	echo '<p>' . $msg. '</p>';
-	echo '<p>Installation failed - click <a href="' . $myScriptName. '">here</a> to start over</p>';
+	showInstallFailed ($msg);
 }
 
 ?>
